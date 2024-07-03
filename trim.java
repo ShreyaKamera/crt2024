25c208f6f3abadc449268685cd98e2042750b612
ShreyaKamera <kamerashreya2002@gmail.com>
2024-07-03 14:09:20 +0530

    Create trim.java

---
+++ trim.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Test
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner (System.in);
+        System.out.println(20+40);
+        System.out.println("Wel"+"Come");
+        System.out.println(20+"Python");
+    }
+}
