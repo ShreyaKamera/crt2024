c6a0bd8220b588e46e33a267209badbb8eb5564a
ShreyaKamera <kamerashreya2002@gmail.com>
2024-07-12 09:47:21 +0530

    Create checked exception.java

---
+++ checked exception.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String args[])throws InterruptedException
+    {
+        System.out.println("Java");
+        Thread.sleep(3000);
+        System.out.println("Python");
+    }
+}
